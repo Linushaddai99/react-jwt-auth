0524dd464f4813c26fc233d99012f308b214de43
Linus Shaddai <awajisomegak@gmail.com>
2023-08-17 13:20:57 +0100

    create functionality for auth services

---
+++ src/services/auth.service.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { response } from "express";
+
+const API_URL = "http://localhost:8080/api/auth/";
+
+export default function AuthService()  {
+    const login = (username, password) => {
+        return axios
+        .post(API_URL + 'signin', {
+            username,
+            password
+        })
+        .then(response => {
+            if(response.data.accessToken) {
+                localStorage.setItem('user', JSON.stringify(response.data))
+            }
+            return response.data;
+        })
+
+    }
+
+    const logout = () => {
+        localStorage.removeItem('user')
+    }
+
+    const register = (username, email, password) => {
+        return axios.post(API_URL + 'signup', {
+            username,
+            email,
+            password
+        });
+    }
+
+    const getCurrentUser = () => {
+        return JSON.parse(localStorage.getItem('user'));
+    }
+
+    return {
+        login,
+        logout,
+        register,
+        getCurrentUser
+    }
+}
+
+;
